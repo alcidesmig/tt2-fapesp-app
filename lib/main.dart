8cf0d27d3b528f99abda187c8c895c2ac8c35476
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2020-02-12 16:57:03 -0300

    Mudando para StatefulWidget para alteração dos valores dos sensores em tempo real

--- lib/main.dart
+++ lib/main.dart
@@ -3,79 +3,125 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
+List<String> desc = [
+  'Temperatura',
+  'Velocidade indicada do ar',
+  'Velocidade real do ar',
+  'Umidade',
+  'Bússola',
+  'Latitude',
+  'Longitude',
+  'Altitude'
+];
 
-  List<String> desc = ['Temperatura', 'Velocidade indicada do ar', 'Velocidade real do ar', 'Umidade', 'Bússola', 'Latitude', 'Longitude', 'Altitude'];
-
-  int point = 1;
-
-  runApp(MyApp(
-    items: List<ListItem>.generate(
-      24,
-          (i) => i % 8 == 0
-          ? HeadingItem("Ponto de coleta " + (point++).toString())
-          : MessageItem(desc[i % 8], "Nenhum valor lido ainda"),
-    ),
-  ));
+class SwipeToRefresh extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SwipeToRefreshState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  final List<ListItem> items;
+class _SwipeToRefreshState extends State<SwipeToRefresh> {
 
-  MyApp({Key key, @required this.items}) : super(key: key);
+  SensorsData last_data = SensorsData("Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda");
+
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
+
+  List<ListItem> items = List<ListItem>.generate(
+    24,
+    (i) => i % 8 == 0
+        ? HeadingItem("Ponto de coleta: " + (i ~/ 8 + 1).toString())
+        : MessageItem(desc[i % 8], "Nenhum valor lido ainda"),
+  );
+
+  String title = 'Drone - Visualização de dados';
+
+  Widget buildBody(BuildContext context, int index) {
+    final item = items[index];
+    if (item is HeadingItem) {
+      return ListTile(
+        title: Text(
+          item.heading,
+          style: Theme.of(context).textTheme.headline,
+        ),
+      );
+    } else if (item is MessageItem) {
+      return ListTile(
+        title: Text(item.sender),
+        subtitle: Text(item.body),
+      );
+    }
+  }
+
+  Future<Null> _refresh() {
+    return getData().then((_last_data) {
+      setState(() => last_data = _last_data);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Drone - Visualização de dados';
-
     return MaterialApp(
       title: title,
       home: Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: ListView.builder(
+        body: RefreshIndicator(
+          onRefresh: _refresh,
+          key: _refreshIndicatorKey,
+        child: ListView.builder(
           // Let the ListView know how many items it needs to build.
           itemCount: items.length,
           // Provide a builder function. This is where the magic happens.
           // Convert each item into a widget based on the type of item it is.
-          itemBuilder: (context, index) {
-            final item = items[index];
-
-            if (item is HeadingItem) {
-              return ListTile(
-                title: Text(
-                  item.heading,
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              );
-            } else if (item is MessageItem) {
-              return ListTile(
-                title: Text(item.sender),
-                subtitle: Text(item.body),
-              );
-            }
-          },
+          itemBuilder: (BuildContext context, int index) =>
+              buildBody(context, index),
         ),
+        )
       ),
     );
   }
 }
 
-class DynamicListState extends State {
-  @override
-  Widget build(BuildContext context) {
-    /*items.
+class SensorsData {
+  final String temp,
+      indicated_airspeed,
+      true_airspeed,
+      humidity,
+      compass,
+      lat,
+      long,
+      alt;
 
-    return Text(wordPair.asPascalCase);
-  */}
+  SensorsData(this.temp, this.indicated_airspeed, this.true_airspeed,
+      this.humidity, this.compass, this.lat, this.long, this.alt);
+
+  factory SensorsData.fromJson(Map<String, dynamic> json) {
+    // json = json['results'][0];
+    String temp = json['temp'];
+    String indicated_airspeed = json['indicated_airspeed'];
+    String true_airspeed = json['true_airspeed'];
+    String humidity = json['humidity'];
+    String compass = json['compass'];
+    String lat = json['lat'];
+    String long = json['long'];
+    String alt = json['alt'];
+    return SensorsData(temp, indicated_airspeed, true_airspeed, humidity,
+        compass, lat, long, alt);
+  }
 }
 
-class DynamicList extends StatefulWidget {
-  @override
-  DynamicListState createState() => DynamicListState();
+Future<SensorsData> getData() async {
+  final response = await http.get("https://jsonplaceholder.typicode.com/posts");
+  final responseJson = json.decode(response.body);
+  return SensorsData.fromJson(responseJson);
 }
+
+void main() => runApp(SwipeToRefresh());
 
 // The base class for the different types of items the list can contain.
 abstract class ListItem {}
@@ -94,7 +140,6 @@ class MessageItem implements ListItem {
 
   MessageItem(this.sender, this.body);
 }
-
 
 /*
 
