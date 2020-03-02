28bc9f2b98b3054764640bde7a95460d6e28f1df
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2020-03-01 22:10:58 -0300

    Refazendo sistema de atualização dinâmica dos dados utilizando list

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class SwipeToRefresh extends StatefulWidget {
 class _SwipeToRefreshState extends State<SwipeToRefresh> {
 
 
-  SensorsData last_data = SensorsData("Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", "Nenhum valor lido ainda", []);
+  SensorsData last_data = SensorsData([["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"],["Nenhum valor lido ainda","Nenhum valor lido ainda","Nenhum valor lido ainda"]]);
 
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       new GlobalKey<RefreshIndicatorState>();
@@ -54,6 +54,7 @@ class _SwipeToRefreshState extends State<SwipeToRefresh> {
 
   static int cont2 = 0;
   Widget buildBody(BuildContext context, int index) {
+
     final item = items[index];
     if (item is HeadingItem) {
       return ListTile(
@@ -63,10 +64,10 @@ class _SwipeToRefreshState extends State<SwipeToRefresh> {
         ),
       );
     } else if (item is MessageItem) {
-      print("Rebuilding");
+      print(cont2.toString() + "\n");
       return ListTile(
         title: Text(item.sender),
-        subtitle: Text(last_data.values[cont2++ % 8]),
+        subtitle: Text(last_data.values[cont2 % 8][cont2++ ~/ 8]),
       );
     }
   }
@@ -78,7 +79,8 @@ class _SwipeToRefreshState extends State<SwipeToRefresh> {
   Future<Null> _refresh() {
     return getData().then((_last_data) {
       this.setState(() => this.last_data = _last_data );
-      print(last_data.temp);
+      print(last_data.values);
+      cont2 = 0;
     });
   }
 
@@ -108,32 +110,50 @@ class _SwipeToRefreshState extends State<SwipeToRefresh> {
 }
 
 class SensorsData {
-  final String temp,
-      indicated_airspeed,
-      true_airspeed,
-      humidity,
-      compass,
-      lat,
-      long,
-      alt;
 
-  final List<String> values;
+  final List<List<String>> values;
 
-  SensorsData(this.temp, this.indicated_airspeed, this.true_airspeed,
-      this.humidity, this.compass, this.lat, this.long, this.alt, this.values);
+  SensorsData(this.values);
 
   factory SensorsData.fromJson(Map<String, dynamic> json) {
-    // json = json['results'][0];
-    String temp = json['temp'] != null ? json['temp'] : "Nenhum valor lido ainda 2";
-    String indicated_airspeed = json['indicated_airspeed'] != null ? json['indicated_airspeed'] : "Nenhum valor lido ainda";
-    String true_airspeed = json['true_airspeed'] != null ? json['true_airspeed'] : "Nenhum valor lido ainda";
-    String humidity = json['humidity'] != null ? json['humidity'] : "Nenhum valor lido ainda";
-    String compass = json['compass'] != null ? json['compass'] : "Nenhum valor lido ainda";
-    String lat = json['lat'] != null ? json['lat'] : "Nenhum valor lido ainda2";
-    String long = json['long'] != null ? json['long'] : "Nenhum valor lido ainda";
-    String alt = json['alt'] != null ? json['alt'] : "Nenhum valor lido ainda";
-    return SensorsData(temp, indicated_airspeed, true_airspeed, humidity,
-        compass, lat, long, alt, [temp, indicated_airspeed, true_airspeed, humidity,
+    String temp_0 = json['temp_0'] != null ? json['temp_0'] : "Nenhum valor lido ainda";
+    String indicated_airspeed_0 = json['indicated_airspeed_0'] != null ? json['indicated_airspeed_0'] : "Nenhum valor lido ainda";
+    String true_airspeed_0 = json['true_airspeed_0'] != null ? json['true_airspeed_0'] : "Nenhum valor lido ainda";
+    String humidity_0 = json['humidity_0'] != null ? json['humidity_0'] : "Nenhum valor lido ainda";
+    String compass_0 = json['compass_0'] != null ? json['compass_0'] : "Nenhum valor lido ainda";
+    String lat_0 = json['lat_0'] != null ? json['lat_0'] : "Nenhum valor lido ainda";
+    String long_0 = json['long_0'] != null ? json['long_0'] : "Nenhum valor lido ainda1";
+    String alt_0 = json['alt_0'] != null ? json['alt_0'] : "Nenhum valor lido ainda1";
+
+    String temp_1 = json['temp_1'] != null ? json['temp_1'] : "Nenhum valor lido ainda";
+    String indicated_airspeed_1 = json['indicated_airspeed_1'] != null ? json['indicated_airspeed_1'] : "Nenhum valor lido ainda";
+    String true_airspeed_1 = json['true_airspeed_1'] != null ? json['true_airspeed_1'] : "Nenhum valor lido ainda";
+    String humidity_1 = json['humidity_1'] != null ? json['humidity_1'] : "Nenhum valor lido ainda";
+    String compass_1 = json['compass_1'] != null ? json['compass_1'] : "Nenhum valor lido ainda";
+    String lat_1 = json['lat_1'] != null ? json['lat_1'] : "Nenhum valor lido ainda";
+    String long_1 = json['long_1'] != null ? json['long_1'] : "Nenhum valor lido ainda21";
+    String alt_1 = json['alt_1'] != null ? json['alt_1'] : "Nenhum valor lido ainda21";
+
+    String temp_2 = json['temp_2'] != null ? json['temp_2'] : "Nenhum valor lido ainda";
+    String indicated_airspeed_2 = json['indicated_airspeed_2'] != null ? json['indicated_airspeed_2'] : "Nenhum valor lido ainda";
+    String true_airspeed_2 = json['true_airspeed_2'] != null ? json['true_airspeed_2'] : "Nenhum valor lido ainda";
+    String humidity_2 = json['humidity_2'] != null ? json['humidity_2'] : "Nenhum valor lido ainda";
+    String compass_2 = json['compass_2'] != null ? json['compass_2'] : "Nenhum valor lido ainda";
+    String lat_2 = json['lat_2'] != null ? json['lat_2'] : "Nenhum valor lido ainda";
+    String long_2 = json['long_2'] != null ? json['long_2'] : "Nenhum valor lido ainda31";
+    String alt_2 = json['alt_2'] != null ? json['alt_2'] : "Nenhum valor lido ainda31";
+
+    var temp = [temp_0, temp_1, temp_2];
+    var indicated_airspeed = [indicated_airspeed_0, indicated_airspeed_1, indicated_airspeed_2];
+    var true_airspeed = [true_airspeed_0, true_airspeed_1, true_airspeed_2];
+    var humidity = [humidity_0, humidity_1, humidity_2];
+    var compass = [compass_0, compass_1, compass_2];
+    var lat = [lat_0, lat_1, lat_2];
+    var long = [long_0, long_1, long_2];
+    var alt = [alt_0, alt_1, alt_2];
+
+
+    return SensorsData([temp, indicated_airspeed, true_airspeed, humidity,
           compass, lat, long, alt]);
   }
 }
